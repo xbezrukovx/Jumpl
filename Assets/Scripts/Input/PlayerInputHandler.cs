4b3282b40638ff9c7a0539d5cbe04ffeca811baa
Denis <ds.bezrukov@icloud.com>
2024-01-07 16:01:25 +0500

    Tower Rotation has been created

---
+++ Assets/Scripts/Input/PlayerInputHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Tower;
+using UnityEngine;
+
+namespace Input
+{
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        [Header("Input")]
+        [SerializeField] private InputSwipePanel _swipePanel;
+        
+        [Header("Tower")]
+        [SerializeField] private TowerRotation _towerRotation;
+
+        private void OnEnable()
+        {
+            _swipePanel.Swiping += RotateTower;
+        }
+
+        private void OnDisable()
+        {
+            _swipePanel.Swiping -= RotateTower;
+        }
+
+        private void RotateTower(Swipe swipe)
+        {
+            float xAxis = swipe.Delta.x;
+            _towerRotation.Rotate(xAxis);
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/TowerRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Tower
+{
+    public class TowerRotation : MonoBehaviour
+    {
+        [SerializeField] [Min(0.0f)] private float _rotationSpeed;
+        [SerializeField] private Rigidbody _rigidbody;
+        public void Rotate(float xAxis)
+        {
+            Vector3 torque = Vector3.up * xAxis * Time.deltaTime * _rotationSpeed * -1;
+            _rigidbody.AddTorque(torque, ForceMode.Acceleration);
+        }
+    }
+}
