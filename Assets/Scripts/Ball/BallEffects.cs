2a4f4a581a59074f6423282d97e32729043235a3
Denis <ds.bezrukov@icloud.com>
2024-01-07 18:22:21 +0500

    Collision particles

---
+++ Assets/Scripts/Ball/BallEffects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ball
+{
+    public class BallEffects : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _collisionParticlesPrefab;
+        private void OnCollisionEnter(Collision other)
+        {
+            Vector3 hitPoint = other.contacts[0].point;
+            EmitCollisionParticles(hitPoint);
+        }
+
+        private void EmitCollisionParticles(Vector3 hitPoint)
+        {
+            Instantiate(_collisionParticlesPrefab, hitPoint, Quaternion.identity);
+        }
+    }
+}
