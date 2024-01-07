3ce80ca08a53bc6e4564fafc62600ab23e621088
Denis <ds.bezrukov@icloud.com>
2024-01-08 02:36:50 +0500

    Ball bounce created

---
+++ Assets/Scripts/Ball/BallBounce.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Physics;
+using Structures;
+using UnityEngine;
+
+namespace Ball
+{
+    public class BallBounce : MonoBehaviour
+    {
+        [Header("Required components")]
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private Transform _mesh;
+        
+        [Header("Settings")]
+        [SerializeField] private BounceData _bounceData;
+        [SerializeField] private Vector3Curves _scaleCurves;
+        
+        private Bounce _bounce;
+        private BounceEffects _bounceEffects;
+
+        private Vector3 _meshInitialScale;
+        private void Start()
+        {
+            _bounce = new Bounce(_rigidbody, _bounceData);
+            _bounceEffects = new BounceEffects(_rigidbody, _bounceData, _scaleCurves);
+            _meshInitialScale = _mesh.localScale;
+        }
+
+        private void FixedUpdate()
+        {
+            _bounce.ClampHeight();
+            _bounceEffects.ApplyUpwardsScaleTo(_mesh, _meshInitialScale);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            _bounce.BounceOff(Vector3.up);
+        }
+    }
+}
---
+++ Assets/Scripts/Structures/Vector3Curves.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Structures
+{
+    [System.Serializable]
+    public class Vector3Curves
+    {
+        [SerializeField] private AnimationCurve _xCurve;
+        [SerializeField] private AnimationCurve _yCurve;
+        [SerializeField] private AnimationCurve _zCurve;
+
+        public AnimationCurve XCurve => _xCurve;
+
+        public AnimationCurve YCurve => _yCurve;
+
+        public AnimationCurve ZCurve => _zCurve;
+    }
+}
